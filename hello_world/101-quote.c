8b67cf1fb24d62212d67f09f87068a9873760e37
Ivanzkaa <ivanzka@me.com>
2022-06-06 19:14:24 +0000

    writing

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to prin it out
+ * Return: will come out to 1
+ **/
+int main(void)
+{
+	wrtite("\"and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+	return (1);
+}
