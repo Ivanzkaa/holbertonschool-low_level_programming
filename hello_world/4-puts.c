586f595987aa08841c513857396b6f86da3d06bd
Ivanzkaa <ivanzka@me.com>
2022-06-06 17:51:18 +0000

    using the puts fucntion

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main () {
 	/* using the puts function */
    char str1[55];
 
-   strcpy(str1, "Programming is like building a multilingual puzzle\n");
+   strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
 
    puts(str1);
    
