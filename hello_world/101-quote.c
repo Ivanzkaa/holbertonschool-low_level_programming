1ca1278814a4a77f68a8c7e3de09b0e866e24536
Ivanzkaa <ivanzka@me.com>
2022-06-07 03:47:58 +0000

    the write function

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - to prin it out
  * Return: will come out to 1
+ * m - the name of the variable
  **/
 int main(void)
 {
-	wrtite("\"and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+
 	return (1);
 }
