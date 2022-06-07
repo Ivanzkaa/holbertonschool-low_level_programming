429f8290fa45134163bf8335edd9ff946e011dee
Ivanzkaa <ivanzka@me.com>
2022-06-07 20:47:08 +0000

    basically the same output

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,11 @@ int main(void)
 {
 	char ch;
 
-	printf("Uppercase characters : \n");
-
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
 
-	printf("\nLowercase characters : \n");
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
