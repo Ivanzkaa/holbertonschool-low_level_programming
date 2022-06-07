3b8d04d115fd8a50ccac3889f39921f95e1f3a37
Ivanzkaa <ivanzka@me.com>
2022-06-07 20:09:36 +0000

    almost the same way as listing items

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - to print out the function
+ * Return: will come out to 0
+ **/
+int main(void)
+{
+	char ch;
+
+	printf("\nLowercase characters : \n");
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		printf("%c ", ch);
+	}
+
+	return (0);
+}
