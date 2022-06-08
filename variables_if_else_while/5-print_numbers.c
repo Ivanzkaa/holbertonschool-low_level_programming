4c022264404d244d97e7b1c5e052eadec20f0706
Ivanzkaa <ivanzka@me.com>
2022-06-08 17:59:59 +0000

    #

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,28 +6,7 @@
  **/
 int main(void)
 {
-
-	int num;
-	int i = 0;
-	int arr[MAX];
-	int j, r;
-
-	while (num != 0)
-	{
-
-		(r = num % 10);
-
-		(arr[i] = r);
-		(i++);
-
-		num = num / 10;
-	}
-
-	for (j = i - 1; j > -1; j--)
-	{
-		printf("%d", arr[j]);
-
-	}
-	putchar('\n');
+	printf("0123456789\n");
 	return (0);
+
 }
