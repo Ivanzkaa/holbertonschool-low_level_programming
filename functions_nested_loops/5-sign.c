220382fbf2b87cc81a062e4c1579d450d367aed0
Ivanzkaa <ivanzka@me.com>
2022-06-09 18:23:29 +0000

    -

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,24 +2,25 @@
 /**
  * print_sign - to print out the function
  * @n: is part of the print function
- * Return: will come out to 1, 0 ,1
+ * Return: will come out to 0
  **/
 int print_sign(int n)
 {
 
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
