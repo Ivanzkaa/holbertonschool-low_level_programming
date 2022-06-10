862eb3201c622173124f1808d5305f03426fc105
Ivanzkaa <ivanzka@me.com>
2022-06-10 14:05:37 +0000

    changes to the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - to print out the function
+ * Return: will come out to n
+ * @n: variable
+ **/
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+
+	else
+	{
+		(n *= -1);
+		return (-n);
+	}
+}
