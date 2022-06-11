5ee33ef74e7296761b8ae6997ddb5f7e75d68c96
Ivanzkaa <ivanzka@me.com>
2022-06-11 02:33:00 +0000

    the addition of the 2 integers and the return value of the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - prints out the function of adding
+* @a: first integer
+* @b: seconf integer
+* Return: its adding a + b
+**/
+int add(int a, int b)
+{
+	return (a + b);
+}
