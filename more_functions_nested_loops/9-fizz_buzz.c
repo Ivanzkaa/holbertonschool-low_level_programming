15a8a830fd29e33044b5f737397f488dcd6b4e1a
Ivanzkaa <ivanzka@me.com>
2022-06-14 20:26:50 +0000

    doing fizzbuzz again but a slight different format that just require more specs like the else statements and printing all together

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *main - to print ou the function
+ *Return: will come out to 0
+ **/
+int main(void)
+{
+	int num = 100;
+	int i;
+
+	i = 1;
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i < num)
+
+				printf("Buzz ");
+
+			else
+				printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
