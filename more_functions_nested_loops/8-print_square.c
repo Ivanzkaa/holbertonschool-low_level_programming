5bbd070dc86ab4f54b3bf23d5a06514b73cbb3a7
Ivanzkaa <ivanzka@me.com>
2022-06-14 18:03:39 +0000

    again doing almost the same format as the last 2, also adding more whiles

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_square - to print out the function
+ *@size: the variable of the task
+ *Return: void
+ **/
+void print_square(int size)
+{
+
+	int square, square2;
+
+	if (size > 0)
+	{
+		square2 = 0;
+
+		while (square2 < size)
+		{
+			square = 0;
+			while (square < size)
+			{
+				_putchar('#');
+				square++;
+			}
+			putchar('\n');
+			square2++;
+		}
+	} 
+	else
+		_putchar('\n');
+}
