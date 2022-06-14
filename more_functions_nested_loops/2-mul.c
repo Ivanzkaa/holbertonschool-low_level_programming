9e61b5a8f055fbbd145e39e5e0ac769571a1cd59
Ivanzkaa <ivanzka@me.com>
2022-06-14 13:24:13 +0000

    multiplying two integers to get their answer in this function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - will print out the function of the code
+ *@a: the first variable
+ *@b: the second variable
+ *Return: will come out to 0
+ **/
+int mul(int a, int b)
+{
+		return (a * b);
+}
