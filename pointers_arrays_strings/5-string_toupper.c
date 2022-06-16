5a57147885513800611bd536f81f84cbd9add1a5
Ivanzkaa <ivanzka@me.com>
2022-06-16 16:05:35 +0000

    changing the lower case letters to upper using the same function as before

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - change to uppercase
+ * @s: string
+ * Return: value to upper
+ **/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+	return (s);
+}
