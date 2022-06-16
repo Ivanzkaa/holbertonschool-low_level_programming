1d28d6f3998e6728433ce47e564aaa817bbe0b92
Ivanzkaa <ivanzka@me.com>
2022-06-16 18:32:53 +0000

    replacing letters with numbers

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,12 +6,12 @@
  **/
 char *leet(char *s)
 {
-	int i = 0;
+	int i;
 	int n;
 	char old[] = "aAeEoOtTlL";
 	char new[] = "4433007711";
 
-	for (i = 0; *(s + 1); i++)
+	for (i = 0; *(s + i); i++)
 	{
 		for (n = 0; n <= 9; n++)
 		{
