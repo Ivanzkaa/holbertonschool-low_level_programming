92b77dcafbd5cc7062058db631df1cd1ac700e5d
Ivanzkaa <ivanzka@me.com>
2022-06-16 15:58:25 +0000

    reversing strings

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reversing strings
+ * @a: string
+ * @n: string
+ * Return: void
+ **/
+void reverse_array(int *a, int n)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		c = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = c;
+	}
+}
