c540ad9f56c076308708fb188dd3c3df88221c5a
Ivanzkaa <ivanzka@me.com>
2022-06-23 16:59:17 +0000

    cointing the # of arguments that are passed into tge code, if it's in the () it will coint what's inside as on character

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the # of arguments passed into it
+ * @argc: will count the arguments
+ * @argv: point to the array of arguments
+ * Return: will come out to 0
+ **/
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
