a3af3b162ef3c85549f81dc773f47a41da9fa552
Ivanzkaa <ivanzka@me.com>
2022-06-23 16:40:38 +0000

    printing its name withe the argc and the argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - will print out hte function
+ * @argc: will count how many arguments
+ * @argv: pointer to array
+ * Return: will come out to 0
+ **/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
