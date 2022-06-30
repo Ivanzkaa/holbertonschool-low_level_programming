c081d473cf65f70c24f57238e4edfac059c54599
Ivanzkaa <ivanzka@me.com>
2022-06-30 16:43:10 +0000

    printing out the struct of doggo

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - printing struct dog
+ * @d: doggo
+ * Return: void
+ **/
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+
+	if (d->age >= 0)
+		printf("Age: %f\n", d->age);
+
+	(d->owner) ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
+}
