f1caf235a3f029073b87309899f1bb87c4062dcf
Ivanzkaa <ivanzka@me.com>
2022-07-16 20:26:01 +0000

    printing the number of the elements of a linked list

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/** listint_len - number of elements of a linked list
+ * @h: head
+ * Return: the numberof elements
+ **/
+size_t print_listint(const listint_t *h)
+{
+	size_t num_elem = 0;
+
+	for (; h != NULL; num_elem++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (num_elem);
+}
